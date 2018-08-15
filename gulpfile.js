e8f1dd57692e3c5eb099faabac4dfdae5e4f42ed
Xinqian liu <crystalynliu@163.com>
2018-08-15 10:59:11 +0800

    Add week2 and week3 source file.

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,29 @@ gulp.task('week1-server', function() {
   });
 });
 
+gulp.task('week2-server', function() {
+  bs.init({
+    server: {
+      baseDir: "./week2"
+    },
+    port: 8002
+  });
+});
+
+gulp.task('week3-server', function() {
+  bs.init({
+    server: {
+      baseDir: "./week3"
+    },
+    port: 8003
+  });
+});
+
 gulp.task('watch', function () {
   bs.watch('*/*.html').on('change', bs.reload);
   bs.watch('*/*.css').on('change', bs.reload);
 })
 
 gulp.task('week1', ['week1-server', 'watch']);
+gulp.task('week2', ['week2-server', 'watch']);
+gulp.task('week3', ['week3-server', 'watch']);
